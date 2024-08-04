edc9f4c7a9537eae3121e0be9954399ec20dd4cd
Bittu Kumar <bk7355583@gmail.com>
2024-08-04 23:59:18 +0530

    db connected

---
+++ server/src/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+app.use(cors({
+    origin: '*', // Allows all origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
+  
+  app.use(express.json({ limit: "16kb" }));
+  app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+  app.use(express.static("public"));
+
+
+  export default app;
